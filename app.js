c90939174a066546bec33c4a2d5756b2c3c5d2f2
TetRoiS <tetrois@ya.ru>
2021-12-22 18:36:12 +0300

    on/off logs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const fetch = require('node-fetch');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-let CONFIG = {}
+let CONFIG = {};
 
 let messageSend = false;
 let authData = {};
@@ -57,32 +57,32 @@ async function runParse() {
         messageSend = CONFIG.TG_MESSAGE_SEND;
 
     } catch (error) {
-        console.log("[Main] -> Error");
+        debug_log("[Main] -> Error");
         console.error(error);
         sendMessageTG(msgError(error, `runParse`), CONFIG.TG_CHAT_DEBUG);
     } finally {
-        console.log('All Complete');
+        debug_log('All Complete');
         sendMessageTG(['All%20Complete'], CONFIG.TG_CHAT_DEBUG);
     }
 };
 
 async function checkCookie() {
-    console.log('[Auth] Check has cookie: ', authData.hasOwnProperty('sbs_session') && authData.hasOwnProperty('XSRF_TOKEN'));
+    debug_log('[Auth] Check has cookie: ', authData.hasOwnProperty('sbs_session') && authData.hasOwnProperty('XSRF_TOKEN'));
     if(authData.hasOwnProperty('sbs_session') && authData.hasOwnProperty('XSRF_TOKEN')) {
         let status = !((authData.expiry - Date.now()) >= 0);
-        console.log('[Auth] Check cookie expiration Date: ', status );
+        debug_log('[Auth] Check cookie expiration Date: ', status );
         if ( status ){
-            console.log('[Auth] Get cookie process start');
+            debug_log('[Auth] Get cookie process start');
             await login();
             
-            console.log('[Auth] Done');
+            debug_log('[Auth] Done');
         } else {
-            console.log('[Auth] Cookie Good');
+            debug_log('[Auth] Cookie Good');
         }
     } else { 
-        console.log('[Auth] Get cookie process start');
+        debug_log('[Auth] Get cookie process start');
         await login();
-        console.log('[Auth] Done');
+        debug_log('[Auth] Done');
     }
 }
 
@@ -96,7 +96,7 @@ function getDate() {
         dd = dd < 10 ? `0${dd}` : dd;
         mm = mm < 10 ? `0${mm}` : mm;
         let nowDate = yyyy + "-" + mm + "-" + dd;
-        console.log("Today is: " + nowDate);
+        debug_log("Today is: " + nowDate);
         return nowDate;
     } catch (error) {
         console.error(error);
@@ -112,10 +112,10 @@ async function getFileData(fileName) {
             messageSend = false;
             sendMessageTG("sendMessageOff", CONFIG.TG_CHAT_DEBUG);
         };
-        console.log(`[Read Old Save Data] -> File ${fileName} Read Good =)`);
+        debug_log(`[Read Old Save Data] -> File ${fileName} Read Good =)`);
         return fileData;
     } catch (error) {
-        console.log(`[Read Old Save Data] -> Error Read File ${fileName}. Using empty data.`);
+        debug_log(`[Read Old Save Data] -> Error Read File ${fileName}. Using empty data.`);
         messageSend = false;
         sendMessageTG(msgError(null, `Creating empty file ${fileName}`), CONFIG.TG_CHAT_DEBUG);
         return fileData = {}
@@ -125,9 +125,9 @@ async function getFileData(fileName) {
 async function writeDataRE(text, nowDate, type) {
     try {
         await fs.writeFile(`./updates_data/${type}${nowDate}.json`, JSON.stringify(text));
-        console.log('[Save Data] -> Done');
+        debug_log('[Save Data] -> Done');
     } catch (error) {
-        console.log('[Save Data] -> Error: ');
+        debug_log('[Save Data] -> Error: ');
         console.error(error);
         sendMessageTG(msgError(error, "writeDataRE"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -136,9 +136,9 @@ async function writeDataRE(text, nowDate, type) {
 async function writeData(text) {
     try {
         await fs.writeFile(CONFIG.PATH_FILE_OLD_DATA, JSON.stringify(text));
-        console.log('[Save Data] -> Done');
+        debug_log('[Save Data] -> Done');
     } catch (error) {
-        console.log('[Save Data] -> Error: ');
+        debug_log('[Save Data] -> Error: ');
         console.error(error);
         sendMessageTG(msgError(error, "writeData"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -150,7 +150,7 @@ async function getUpdateData(nowDate, type) {
         return data = await getFileData(`./updates_data/${type}${nowDate}.json`)
     } catch (error) {
         messageSend = false;
-        console.log(`[Read ${type} Data] -> File used release not found. Will be using empty data.`);
+        debug_log(`[Read ${type} Data] -> File used release not found. Will be using empty data.`);
         sendMessageTG(msgError(null, `Create empty file ./updates_data/${type}${nowDate}.json`), CONFIG.TG_CHAT_DEBUG);
         return data = { updates: {}, videorelease_id: {} };
     }
@@ -159,7 +159,7 @@ async function getUpdateData(nowDate, type) {
 //Login
 async function login() {
     try {
-        console.log('[Login] -> Start');
+        debug_log('[Login] -> Start');
 
         let initResponse = await getApiData(CONFIG.RB_LOGIN_LINK, true);
         setAuthData(initResponse);
@@ -167,10 +167,10 @@ async function login() {
         let authResponse = await getApiData(CONFIG.RB_API_LOGIN_LINK, true, "json", "POST", `{"password":"${CONFIG.RB_AUTH_PASSWORD}","user_login":"${CONFIG.RB_AUTH_LOGIN}"}`);
         setAuthData(authResponse);
 
-        console.log('[Login] -> Complete');
+        debug_log('[Login] -> Complete');
             
     } catch (error) {
-        console.log("[Login] -> Error");
+        debug_log("[Login] -> Error");
         console.error(error);
         sendMessageTG(msgError(error, "login"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -202,7 +202,7 @@ async function getApiData(link, returnResponse = false, typeReturn, method = "GE
             method: method,
             body: body
         });
-        console.log(`[Get Api Data] -> Requset to ${link} ${response.status}`)
+        debug_log(`[Get Api Data] -> Requset to ${link} ${response.status}`)
         if (response.ok) {
             let data;
             if (returnResponse) { return response };
@@ -210,10 +210,10 @@ async function getApiData(link, returnResponse = false, typeReturn, method = "GE
             if (typeReturn === 'text') { data = await response.text() };
             return data;
         } else {
-            console.log(`[Get Api Data] -> Bad response from URL: ${link}  Status: ${response.status}`);
+            debug_log(`[Get Api Data] -> Bad response from URL: ${link}  Status: ${response.status}`);
         }
     } catch (error) {
-        console.log('Error get data ', error);
+        debug_log('Error get data ', error);
         return error;
     }
 }
@@ -222,7 +222,7 @@ async function getApiData(link, returnResponse = false, typeReturn, method = "GE
 async function listCourses() {
 
     try {
-        console.log('[List Courses] -> Start');
+        debug_log('[List Courses] -> Start');
         let coursesData = [];
         let siteData = await getApiData(CONFIG.RB_COURSES_LINK + '&grid-1[page]=1', false, 'text');
         let document  = (new JSDOM(siteData)).window.document;
@@ -253,10 +253,10 @@ async function listCourses() {
             convertData(coursesTable);
         }
 
-        console.log('[List Courses] -> Complete, Total: ' + coursesData.length);
+        debug_log('[List Courses] -> Complete, Total: ' + coursesData.length);
         return coursesData;
     } catch (error) {
-        console.log("[List Courses] -> Error");
+        debug_log("[List Courses] -> Error");
         console.error(error);
         sendMessageTG(msgError(error, "listCourses"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -265,7 +265,7 @@ async function listCourses() {
 
 async function parseSiteData(siteData) {
     try {
-        console.log("[Parse] -> Start"); 
+        debug_log("[Parse] -> Start"); 
                  
         var fn = async function delay(elem) {
             let data = await getApiData(elem.idLink, false, 'json');
@@ -277,10 +277,10 @@ async function parseSiteData(siteData) {
         
         let actions = siteData.map(fn);
         await Promise.all(actions);
-        console.log("[Parse] -> Done");
+        debug_log("[Parse] -> Done");
         return siteData;
     } catch (error) {
-        console.log("[Parse] -> Error");
+        debug_log("[Parse] -> Error");
         console.error(error);
         sendMessageTG(msgError(error, "parseSiteData"), CONFIG.TG_CHAT_DEBUG);
         return error;
@@ -290,15 +290,15 @@ async function parseSiteData(siteData) {
 
 async function getDataRE(linkNames, type, what) {
     try {
-        //console.log(`[Parse ${type} ${what}] -> Start`);
-        console.log(`[Parse ${type} ${what}] -> URL: ${linkNames}`);
+        //debug_log(`[Parse ${type} ${what}] -> Start`);
+        debug_log(`[Parse ${type} ${what}] -> URL: ${linkNames}`);
         let listNames = [];
         listNames = await getApiData(linkNames, false, 'json');
-        console.log(`[Parse ${type} ${what}] -> Count Release: ${listNames.data.length}`);
-        //console.log(`[Parse ${type} ${what}] -> Done`);
+        debug_log(`[Parse ${type} ${what}] -> Count Release: ${listNames.data.length}`);
+        //debug_log(`[Parse ${type} ${what}] -> Done`);
         return listNames;
     } catch (error) {
-        console.log(`[Parse ${type} ${what}] -> Error`);
+        debug_log(`[Parse ${type} ${what}] -> Error`);
         console.error(error);
         sendMessageTG(msgError(error, `getNamesRE`), CONFIG.TG_CHAT_DEBUG);
     }
@@ -348,10 +348,10 @@ async function getUpdatedRE(usedRE, allTodayUpdates, listAllNames, type, nowDate
             }
         }
         await writeDataRE(usedRE, nowDate, type);
-        console.log(`[Get Upd ${type}] -> Done`);
+        debug_log(`[Get Upd ${type}] -> Done`);
         return newReleases;
     } catch (error) {
-        console.log(`[Get Upd ${type}] -> Error`);
+        debug_log(`[Get Upd ${type}] -> Error`);
         console.error(error);
         sendMessageTG(msgError(error, "getUpdatedRelease"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -359,31 +359,31 @@ async function getUpdatedRE(usedRE, allTodayUpdates, listAllNames, type, nowDate
 
 function compareData(siteData, oldSiteData) {
     try {
-        console.log('[Compare] -> Start');
+        debug_log('[Compare] -> Start');
         let newSiteData = [];
         if(siteData.length === oldSiteData.length) {
             for (let a = 0; a < siteData.length; a++) {
                 try {
                     if (oldSiteData[a].id === siteData[a].id) {
-                        console.log(`[Compare] -> ${a}) id: ${oldSiteData[a].idCourse}/${siteData[a].idCourse}  | Version: ${oldSiteData[a].id_update}/${siteData[a].id_update}`);
+                        debug_log(`[Compare] -> ${a}) id: ${oldSiteData[a].idCourse}/${siteData[a].idCourse}  | Version: ${oldSiteData[a].id_update}/${siteData[a].id_update}`);
                         if (oldSiteData[a].id_update !== siteData[a].id_update) {
                             newSiteData.push(siteData[a]);
                         }
                     }
                 } catch (error) {
-                    console.log('[Compare] -> Name Not Found');
+                    debug_log('[Compare] -> Name Not Found');
                 }
             }
         } else {
-            console.log('[Compare] -> Add new course');
+            debug_log('[Compare] -> Add new course');
             messageSend = false;
             sendMessageTG([`Add%20new%20course%20Total ${siteData.length}`], CONFIG.TG_CHAT_DEBUG);
             return siteData;
         }
-        console.log('[Compare] -> Done');
+        debug_log('[Compare] -> Done');
         return newSiteData;
     } catch (error) {
-        console.log("[Compare] -> Error");
+        debug_log("[Compare] -> Error");
         console.error(error);
         sendMessageTG(msgError(error, "compareData"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -391,7 +391,7 @@ function compareData(siteData, oldSiteData) {
 
 function createMessageCourses(newSiteData, send) {
     try {
-        console.log("[Create Message] -> Start");
+        debug_log("[Create Message] -> Start");
         let msg = [];
         if (send) {
             for (let h = 0; h < newSiteData.length; h++) {
@@ -403,12 +403,12 @@ function createMessageCourses(newSiteData, send) {
             if(newSiteData.length > 0){
                 sendMessageTG([`New%20courses%20 ${newSiteData.length}`], CONFIG.TG_CHAT_DEBUG);
             }
-            console.log(`[Create Message] -> Done. Count: ${newSiteData.length}`);
+            debug_log(`[Create Message] -> Done. Count: ${newSiteData.length}`);
         }
         messageSend = true;
         return msg;
     } catch (error) {
-        console.log("[Create Message] -> Error");
+        debug_log("[Create Message] -> Error");
         console.error(error);
         sendMessageTG(msgError(error, "createMessageCourses"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -419,7 +419,7 @@ function createMessageRE(newReleases, type, send) {
 
         let typeCourse = (type === 'R') ? 'rb_videorelease' : 'rb_express_education';
 
-        //console.log(`[Create Message ${type}] -> Start`);
+        //debug_log(`[Create Message ${type}] -> Start`);
         let msg = [];
 
         if (send) {
@@ -435,19 +435,19 @@ function createMessageRE(newReleases, type, send) {
                 sendMessageTG([`New%20${type}%20 ${msg.length}`], CONFIG.TG_CHAT_DEBUG);
             }
 
-            console.log(`[Create Message ${type}] -> Done. Count: ${msg.length}`);
+            debug_log(`[Create Message ${type}] -> Done. Count: ${msg.length}`);
         }
         //messageSend = true;
         return msg;
     } catch (error) {
-        console.log(`[Create Message ${type}] -> Error`);
+        debug_log(`[Create Message ${type}] -> Error`);
         console.error(error);
         sendMessageTG(msgError(error, "createMessageRE"), CONFIG.TG_CHAT_DEBUG);
     }
 }
 
 function sendMessageTG(message, chat) {
-    //console.log("[Send Message] -> Start");
+    //debug_log("[Send Message] -> Start");
     try {
         let message_string = "";
         message.forEach( el => {
@@ -460,9 +460,9 @@ function sendMessageTG(message, chat) {
         //    http.post(`https://api.telegram.org/bot${CONFIG.TG_TOKEN}/sendMessage?chat_id=${chat}&parse_mode=html&text=${message[i]}`);
         // }
 
-        //console.log("[Send Message] -> Done");
+        //debug_log("[Send Message] -> Done");
     } catch (error) {
-        console.log("[Send Message] -> Error");
+        debug_log("[Send Message] -> Error");
         console.error(error);
         //sendMessageTG(msgError(error, "sendMessageTG"), CONFIG.TG_CHAT_DEBUG);
     }
@@ -470,15 +470,21 @@ function sendMessageTG(message, chat) {
 
 function msgError(e, f) {
     let msg = ["%23error" + encodeURI(`\nfunc: ${f}\nError text: ${e}`)];
-    //console.log(msg);
+    //debug_log(msg);
     return msg;
 }
 async function makeUpdateFolder() {
     try {
         await fs.mkdir("./updates_data/");
-        console.log("Update folder create");
+        debug_log("Update folder create");
     } catch (error) {
-        console.log("Update folder find");
+        debug_log("Update folder find");
+    }
+}
+
+function debug_log() {
+    if ( CONFIG.DEBUG === "true" ) { //shit code
+        console.log.apply(this, arguments);
     }
 }
 
